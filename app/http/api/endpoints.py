675694d7d169bdf3aaabdaf2f9c3e1879be50acd
vladislavbondarenko <bondarencko40@gmail.com>
2020-05-24 18:59:56 +0300

    Homepath

--- app/http/api/endpoints.py
+++ app/http/api/endpoints.py
@@ -7,6 +7,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def index():
+    return '<h1> Deployed </h1'
 
 @app.route("/kudos", methods=["GET"])
 @login_required
