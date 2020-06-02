45ebd96569330fb50c89338e79d2609cf3017360
vladislavbondarenko <bondarencko40@gmail.com>
2020-06-02 21:25:21 +0300

    endpoints for leaderBoard

--- app.py
+++ app.py
@@ -10,12 +10,22 @@ import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask_cors import CORS
 from flask_socketio import SocketIO, send, emit
+from flask_pymongo import PyMongo
+from flask import jsonify
+from flask import request
+from pymongo import MongoClient
+from flask import request
+
+import json
+from bson import ObjectId
 
 app = Flask(__name__)
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
+app.config["MONGO_URI"] = "mongodb+srv://vlad123:qwerty123@tic-tac-toe-jtwsd.mongodb.net/scoreboard?retryWrites=true&w=majority"
+# app.config['MONGO_DBNAME'] = 'scoreboard'
+# app.config['SECRET_KEY'] = 'secret_key'
 
 
 ###
@@ -26,6 +36,34 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configur
 def home():
     """Render website's home page."""
     return '<h1> Deployed </h1>'
+
+# returns created instance
+@app.route("/create-score", methods=["POST"])
+def createScore():
+    userName = request.args.get('userName')
+    id = ObjectId()
+    mongo.db.scoreboard.insert({"_id": id, "userName": userName, "winCount": 1})
+    output = []
+    for s in mongo.db.scoreboard.find({"_id": id}):
+        output.append({'userName': s['userName'], 'winCount': s['winCount'], "_id": str(id)})
+    return jsonify({'result': output})
+
+
+@app.route('/set-score', methods=["PUT"])
+def setScore():
+    id = request.args.get("id")
+    value = request.args.get('value')
+    mongo.db.scoreboard.update({"_id": ObjectId(id)}, {"$set": {"winCount": int(value)}})
+    return "done"
+
+@app.route("/get-score")
+def getScore():
+    scores = mongo.db.scoreboard
+    output = []
+    for s in scores.find({}):
+        output.append({'userName': s['userName'],'winCount': s['winCount'], "_id": str(s['_id'])})
+    return jsonify({'result': output})
+
 
 
 @app.route('/about/')
@@ -74,6 +112,15 @@ def handle_json(json):
 @socketio.on('connect')
 def test_connect():
     send('Connected from server')
+
+
+mongo = PyMongo(app)
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
 
 if __name__ == '__main__':
     socketio.run(app, cors_allowed_origins="*")
